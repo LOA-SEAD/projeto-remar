f306e05cccb1b35e474a50e449829be464ded40a
Matheus <matheus.frndes@gmail.com>
2015-03-23 13:26:27 -0300

    GSON plugin

--- ProjetoREMAR/grails-app/conf/BuildConfig.groovy
+++ ProjetoREMAR/grails-app/conf/BuildConfig.groovy
@@ -84,5 +84,7 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
+
+        compile ":gson:1.1.4"
     }
 }
