48e3193843083b6b8f852662b4d8b89ee5225564
Matheus <matheus.frndes@gmail.com>
2015-03-23 13:25:07 -0300

    Apache HttpClient dependency fix

--- ProjetoREMAR/grails-app/conf/BuildConfig.groovy
+++ ProjetoREMAR/grails-app/conf/BuildConfig.groovy
@@ -53,7 +53,11 @@ grails.project.dependency.resolution = {
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 
+        // TODO: remove unnecessary HttpClient dependencies
         build 'org.apache.httpcomponents:httpclient:4.4'
+        runtime 'org.apache.httpcomponents:httpclient:4.4'
+        compile 'org.apache.httpcomponents:httpclient:4.4'
+
     }
 
     plugins {
