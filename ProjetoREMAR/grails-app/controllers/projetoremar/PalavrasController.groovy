40df79a93c01b5f0d664255329287b35b7e69869
Matheus <matheus.frndes@gmail.com>
2015-04-01 11:05:40 -0300

    JSON response on delete method

--- ProjetoREMAR/grails-app/controllers/projetoremar/PalavrasController.groovy
+++ ProjetoREMAR/grails-app/controllers/projetoremar/PalavrasController.groovy
@@ -133,12 +133,19 @@ class PalavrasController {
 
         palavrasInstance.delete flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Palavras.label', default: 'Palavras'), palavrasInstance.id])
-                redirect action:"index", method:"GET"
+        if (request.isXhr()) {
+            render(contentType: "application/json") {
+                palavrasInstance
             }
-            '*'{ render status: NO_CONTENT }
+        } else {
+
+            request.withFormat {
+                form multipartForm {
+                    flash.message = message(code: 'default.deleted.message', args: [message(code: 'Palavras.label', default: 'Palavras'), palavrasInstance.id])
+                    redirect action: "index", method: "GET"
+                }
+                '*' { render status: NO_CONTENT }
+            }
         }
     }
 
