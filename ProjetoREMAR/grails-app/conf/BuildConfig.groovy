a6ce768d60fd46a790e1b80150d30d8542d17ced
Matheus <matheus.frndes@gmail.com>
2015-04-09 13:04:33 -0300

    h2 ~> MySQL fix

--- ProjetoREMAR/grails-app/conf/BuildConfig.groovy
+++ ProjetoREMAR/grails-app/conf/BuildConfig.groovy
@@ -49,9 +49,9 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-//        runtime 'mysql:mysql-connector-java:5.1.34'
+        runtime 'mysql:mysql-connector-java:5.1.34'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
-        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+//        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 
 
     }
