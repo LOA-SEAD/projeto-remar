920f22584760e88ffca1147eeb9b6861bad3f45b
Matheus <matheus.frndes@gmail.com>
2015-05-05 15:24:24 -0300

    Refactoring enhancements + APK publishing

--- QuiForca/grails-app/services/br/ufscar/sead/loa/quiforca/remar/RefactorService.groovy
+++ QuiForca/grails-app/services/br/ufscar/sead/loa/quiforca/remar/RefactorService.groovy
@@ -6,36 +6,26 @@ import org.codehaus.groovy.grails.web.context.ServletContextHolder
 import org.codehaus.groovy.grails.web.util.WebUtils
 
 @Transactional
+
 class RefactorService {
 
     def serviceMethod(Execution execution) {
         def rootPath = ServletContextHolder.getServletContext().getRealPath("/")
-        def sourceFodler = new File(rootPath, "/data/source")
+        rootPath = rootPath.substring(0, rootPath.length() -1)
+        def sourceFodler = "$rootPath/data/source"
         def userId = WebUtils.retrieveGrailsWebRequest().session.userId
         def themeId = WebUtils.retrieveGrailsWebRequest().session.themeId
-        def userFolder = rootPath + "data/$userId"
+        def userFolder = rootPath + "/data/$userId"
 
-        if(sourceFodler.mkdirs()) {
-            "git clone https://github.com/LOA-SEAD/quiforca.git $sourceFodler".execute().waitFor()
-
-            "rm -rf $sourceFodler/.git".execute().waitFor()
-
-            "rm -rf $sourceFodler/json/palavras.json".execute().waitFor()
-
-            "rm -rf $sourceFodler/imgs/inicio.png".execute().waitFor()
-
-            "rm -rf $sourceFodler/imgs/papel.png".execute().waitFor()
-        }
-
-        "cp -R $sourceFodler/ $userFolder/game".execute().waitFor()
+        "cp -R $sourceFodler/ $userFolder/game".execute().waitFor() // TODO: copy only on first user exec
 
         "cp $userFolder/configuracao.json $userFolder/game/json/configuracao.json".execute().waitFor()
-
-        println "cp $userFolder/palavras.json $userFolder/game/json/palavras.json"
 
         "cp $userFolder/themes/$themeId/opening.png $userFolder/game/imgs/inicio.png".execute().waitFor()
 
         "cp $userFolder/themes/$themeId/background.png $userFolder/game/imgs/papel.png".execute().waitFor()
 
+        "$rootPath/scripts/publish_android.sh br.ufscar.sead.loa.forca $userFolder/game/manifest.json $userFolder".execute().waitFor()
     }
 }
+
