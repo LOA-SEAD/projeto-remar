ab700336a760e2d042a3e935f4b35d722dc8e02f
Vinicius Nordi Esperança <viniciusnordiesperanca@gmail.com>
2015-05-08 11:13:24 -0300

    vinculação das atividades e listagem

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/ProfessorController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/ProfessorController.groovy
@@ -1,14 +1,23 @@
 package br.ufscar.sead.loa.remar
 
 import grails.plugin.springsecurity.annotation.Secured
+import org.camunda.bpm.engine.TaskService
+import org.camunda.bpm.engine.task.Task
+import org.camunda.bpm.engine.task.TaskQuery
 
 @Secured(['ROLE_PROF'])
 class ProfessorController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
     def springSecurityService
+    TaskService taskService
 
     def index() {
+
+    	List<Task> tarefas = taskService.createTaskQuery().taskAssignee(springSecurityService.currentUser.camunda_id).list()
+
+    	print tarefas
+
         render view: 'index'
     }
 }
