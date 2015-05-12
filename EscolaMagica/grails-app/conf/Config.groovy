bdad8193be8d8bde31b5043c4cdfd037d35c13fc
Denis <denis_cappelini@hotmail.com>
2015-05-12 13:37:21 -0300

    Botão Logout

--- EscolaMagica/grails-app/conf/Config.groovy
+++ EscolaMagica/grails-app/conf/Config.groovy
@@ -85,6 +85,9 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+grails.plugin.springsecurity.logout.postOnly = false
+logout.afterLogoutUrl = "/"
+
 environments {
     development {
         grails.logging.jul.usebridge = true
