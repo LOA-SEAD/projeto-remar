302426fa3510c31564659b7fb4743639b173cead
Matheus <matheus.frndes@gmail.com>
2015-06-29 10:16:27 -0300

    Process/Deploy API endpoints

--- ProjetoREMAR/grails-app/conf/UrlMappings.groovy
+++ ProjetoREMAR/grails-app/conf/UrlMappings.groovy
@@ -7,10 +7,16 @@ class UrlMappings {
             }
         }
 
+
         "/"(controller:'/index')
         '/user/email/confirm'(controller: 'user',action: 'confirmNewUser')
         '/user/newpassword/confirm'(controller: 'user',action: 'confirmEmail')
         name resetPassword: "/password/reset"(view: "/static/forgottenPassword")
         "500"(view:'/error')
+
+        "/process/task/complete/$id"(controller:"process", action:"completeTask")
+        "/process/task/resolve/$process/$id"(controller:"process", action:"resolveTask")
+        "/process/task/delegate/$process/$id"(controller:"process", action:"delegateTasks")
+        "/deploy/review/$id/$status?"(controller:"deploy", action:"review")
 	}
 }
