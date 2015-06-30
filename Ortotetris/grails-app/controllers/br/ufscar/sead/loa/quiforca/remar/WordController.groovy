bc88d5b77adf7b7f4a171547bf731dee063613cf
marcusmourao <marcus-pf@hotmail.com>
2015-06-30 10:11:58 -0300

    Implementação das demais funções para maniuplar word

--- Ortotetris/grails-app/controllers/br/ufscar/sead/loa/quiforca/remar/WordController.groovy
+++ Ortotetris/grails-app/controllers/br/ufscar/sead/loa/quiforca/remar/WordController.groovy
@@ -16,7 +16,7 @@ class WordController {
         String aux = ""+wordInstance.getAnswer()
         if (wordInstance.getAnswer().length() < 10) {
             for (int i = (10 - wordInstance.getAnswer().length()); i > 0; i--)
-                aux+=("ì");
+                aux+=("ì")
         }
         wordInstance.setWord(aux)
         wordInstance.setInitial_position(0)
@@ -24,12 +24,43 @@ class WordController {
 
     def move_to_left(Word wordInstance) {
         if (wordInstance.getWord().charAt(0) == 'ì') {
-            String aux = wordInstance.getWord().substring(1, 10);
-            aux+=("ì");
+            String aux = wordInstance.getWord().substring(1, 10)
+            aux+=("ì")
             wordInstance.setWord(aux)
             wordInstance.setInitial_position(wordInstance.getInitial_position()-1)
         }
     }//move word para a esquerda
+
+    def move_to_right(Word wordInstance) {
+        if (wordInstance.getWord().charAt(9) == 'ì') {
+            String aux = "ì"
+            aux+=(wordInstance.getWord().substring(0, 9))
+            wordInstance.setWord(aux)
+            wordInstance.setInitial_position(wordInstance.getInitial_position()+1)
+        }
+    }//move word para a direita
+
+    def mark_letter(Word wordInstance, int position) {
+        if ((position-1 >= wordInstance.getInitial_position()) && (position-1 <= wordInstance.getInitial_position() + wordInstance.getAnswer().length()-1)) {
+            String aux
+            aux = wordInstance.getWord().substring(0, position-1)
+            aux+=("0")
+            aux+=(wordInstance.getWord().substring(position, 10))
+            wordInstance.setWord(aux)
+        }
+    }//marca o caractere como '0' (esconde o caractere)
+
+    def clear_position(Word wordInstance, int position) {
+        if ((position-1 >= wordInstance.getInitial_position()) && (position-1 <= wordInstance.getInitial_position() + wordInstance.getAnswer().length()-1)) {
+            String aux
+            aux = wordInstance.getWord().substring(0, position-1)
+            aux+=(wordInstance.getAnswer().charAt(position - initial_position-1))
+            aux+=((wordInstance.getWord().substring(position, 10)))
+            wordInstance.setWord(aux)
+        }
+    }//acessa answer e recupera o caractere que havia sido escondido
+
+
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
--- Ortotetris/grails-app/domain/br/ufscar/sead/loa/quiforca/remar/Word.groovy
+++ Ortotetris/grails-app/domain/br/ufscar/sead/loa/quiforca/remar/Word.groovy
@@ -11,30 +11,5 @@ class Word {
     String answer
     String word
     int initial_position
-
-
-    void mark_letter(int position) {
-        if (position-1 >= initial_position && position-1 <= initial_position + answer.length()-1) {
-            String aux;
-            aux = word.substring(0, position-1);
-            aux+=("0");
-            aux+=(word.substring(position, 10));
-            word = aux;
-
-        }
-    }//marca o caractere como '0' (esconde o caractere)
-
-    void clear_position(int position) {
-        if (position-1 >= initial_position && position-1 <= initial_position + answer.length()-1) {
-            String aux;
-            aux = word.substring(0, position-1);
-            aux+=(answer.charAt(position - initial_position-1));
-            aux+=((word.substring(position, 10)));
-            word = aux;
-        }
-    }//acessa answer e recupera o caractere que havia sido escondido
-
-
-
 }
 
