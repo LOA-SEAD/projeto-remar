7f64a8dcbea539825cbd90a2900e9204e46b30de
Denis <denis_cappelini@hotmail.com>
2015-07-01 10:03:01 -0300

    Remoção Dashboard Controller

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/DashboardController.groovy
+++
@@ -1,9 +0,0 @@
-package br.ufscar.sead.loa.remar
-
-import grails.plugin.springsecurity.annotation.Secured
-
-@Secured(["ROLE_ADMIN","ROLE_STUD","ROLE_USER"])
-class DashboardController {
-
-    def index() { }
-}
