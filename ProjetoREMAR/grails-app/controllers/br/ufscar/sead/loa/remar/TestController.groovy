cda55f9159f61151e5a436cd7d9f2dafaea9dca1
Denis <denis_cappelini@hotmail.com>
2015-07-31 11:34:16 -0300

    Tela de teste para publicação de jogos + JS

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/TestController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/TestController.groovy
@@ -7,67 +7,12 @@ import org.springframework.http.HttpMethod
 @Secured(["IS_AUTHENTICATED_ANONYMOUSLY"])
 class TestController {
     def springSecurityService
-    def facebookGraphService
-    RedisConnection redisHandler
-    def redisService
 
-    def facebookLogin(){
-        println "No test controoler"
-        def details = facebookGraphService.getDetails()
+        def publishGame(){
 
-        if(User.findByFacebookId(details.id)) {
-            def currentUser = User.findByFacebookId(details.id)
-            SpringSecurityUtils.reauthenticate(currentUser.username, currentUser.password)
-            println springSecurityService.getCurrentUser()
 
-            redirect(controller: 'index' ,action: 'index')
-        }
-        else{
-            println session.facebook
-            def acess_token =  session.facebook.access_token
-            // println facebookGraphService.getFacebookProfile(token)
-            //facebookGraphService.api("me",HttpMethod.GET,LogCallback)
-            //println facebookGraphService.getFacebookProfile(session.facebook.access_token)
-            println details.email
-            println details.name
-            println details.id
-            User newUser = new User(
-                    username: details.name,
-                    camunda_id: details.name,
-                    name: details.name,
-                    facebookId: details.id,
-                    email: "Teste2@teste.com.br",
-                    password: details.id,
-                    enabled: true,
-                    accountExpired: false,
-                    accountLocked: false,
-                    passwordExpired: false
-            )
-
-            newUser.save()
-            UserRole.create(newUser, Role.findByAuthority("ROLE_USER"), true)
-            UserRole.create(newUser, Role.findByAuthority("ROLE_FACEBOOK"), true)
-
-            redirect(controller: 'index', action: 'index')
         }
 
-//
-//        FacebookUser fbUser = new FacebookUser(
-//                uid: token.uid,
-//                accessToken:token.accessToken.accessToken,
-//                accessTokenExpires: token.accessToken.expireAt,
-//                user: User.findByUsername("$token.uid")
-//        )
-//        fbUser.save()
 
-    }
 
-    def index() {
-        RedisConnection.reset() // for testing only
-        redisHandler = RedisConnection.getInstance(redisService)
-    }
-
-    def psubscribe() {
-        RedisConnection.getInstance(redisService).pSubscribe("complete*")
-    }
 }
---
+++ ProjetoREMAR/grails-app/views/test/publishGame.gsp
@@ -0,0 +1,40 @@
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <meta name="layout" content="main">
+    <g:javascript src="moodle.js"/>
+    <style>
+    #moodleForm {
+        display: none;
+    }
+    </style>
+    <title></title>
+</head>
+
+<body>
+
+
+<div>
+    <label for="moodle">
+        Deseja publicar para o moodle?
+        <input type="checkbox" id="moodle" name="moodle" value="${false}"/> <br>
+        <a id="moodleForm" target="_blank" href="/moodleGame/gamePublishConfig/">Formulario para o Moodle</a>
+    </label>
+</div>
+<div>
+    <label for="web">
+        Deseja publicar para a Web?
+        <input type="checkbox" id="web" name="web" value="${false}"/> <br>
+        %{--<a id="webLink" target="_blank" href="a" />--}%
+    </label>
+</div>
+<div>
+    <label for="android">
+        Deseja publicar para Android?
+        <input type="checkbox" id="android" name="android" value="${false}"/> <br>
+        %{--<a id="androidLink" target="_blank" />--}%
+    </label>
+</div>
+</body>
+</html>
