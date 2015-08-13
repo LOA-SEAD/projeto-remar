745a41ee13e60551732976fa5da24d3ed72e9806
Matheus <matheus.frndes@gmail.com>
2015-08-13 15:33:01 -0300

    WAR validation and review enhancements

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GameController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GameController.groovy
@@ -2,6 +2,11 @@ package br.ufscar.sead.loa.remar
 
 import grails.converters.JSON
 import grails.plugin.mail.MailService
+import grails.plugins.rest.client.RestBuilder
+import grails.util.Environment
+import groovy.util.slurpersupport.GPathResult
+import groovyx.net.http.HTTPBuilder
+import org.codehaus.groovy.grails.io.support.GrailsIOUtils
 import org.springframework.web.multipart.MultipartFile
 
 import java.security.MessageDigest
@@ -51,6 +56,7 @@ class GameController {
         war.transferTo(file)
 
         // unzip
+        "chmod -R a+x ${servletContext.getRealPath('/scripts')}"
         def unzip = "${servletContext.getRealPath("/scripts")}/unzip.sh ${servletContext.getRealPath("/wars/${session.userId}")} ${fileName}"
         unzip.execute().waitFor()
 
@@ -141,6 +147,9 @@ class GameController {
         gameInstance.bpmn = manifest.bpmn
         gameInstance.comment = "Awaiting review"
 
+        file = new File(servletContext.getRealPath("/wars/${session.userId}"), fileName + ".war")
+                       .renameTo(servletContext.getRealPath("/wars/${session.userId}") + "/" + manifest.uri + ".war")
+
         gameInstance.save flush:true
 
 
@@ -154,15 +163,16 @@ class GameController {
 
     }
 
+    def test() {
+        "chmod a+x ${servletContext.getRealPath("/scripts/test.sh")}".execute().waitFor()
+        render "${servletContext.getRealPath("/scripts/test.sh")}".execute().text
+    }
+
+    @Secured(['ROLE_ADMIN'])
     def review() {
         def gameInstance = Game.findById(params.id)
         String status  = params.status
         String comment = params.comment
-        println gameInstance.id
-        println gameInstance.bpmn
-        println status
-        println comment
-
 
         if(!status) {
             gameInstance.comment = comment
@@ -173,78 +183,75 @@ class GameController {
                     subject "REMAR – O seu WAR \"${gameInstance.name}\" foi rejeitado!"
                     html "<h3>O seu WAR \"${gameInstance.name}\" foi rejeitado pois ${comment}</h3> <br> "
                 }
+                render 'success'
             }
         }
 
         if (status == "approve" && gameInstance.status != "approved") {
 
-            gameInstance.status  = "approved"
-            gameInstance.active = true
-            gameInstance.version = 0
+            "${servletContext.getRealPath("/scripts/db.sh")} ${gameInstance.uri}".execute().waitFor()
 
-            gameInstance.save flush: true
+            if (Environment.current == Environment.DEVELOPMENT) {
+                gameInstance.status  = "approved"
+                gameInstance.active = true
+                gameInstance.version = 0
+                gameInstance.save flush: true
 
-            mailService.sendMail {
-                async true
-                to gameInstance.owner.email
-                subject "REMAR – O seu WAR \"${gameInstance.name}\" foi aprovado!"
-                html '<h3>O seu WAR \"${gameInstance.name}\" foi aprovado! :)</h3> <br>'
+                redirect controller: "process", action: "deploy", id: gameInstance.bpmn
+                return
             }
 
-            redirect controller: "process", action: "deploy", id: gameInstance.bpmn
+            def http = new HTTPBuilder('http://root:root@localhost:8080')
+            def resp = http.get(path: '/manager/text/deploy',
+                    query: [path: "/${gameInstance.uri}",
+                            war: servletContext.getRealPath("/wars/${session.userId}/${gameInstance.uri}.war") ])
+            resp = GrailsIOUtils.toString(resp)
+            if(resp.indexOf('OK') != -1) {
+                gameInstance.status  = "approved"
+                gameInstance.active = true
+                gameInstance.version = 0
+                gameInstance.save flush: true
+
+                mailService.sendMail {
+                    async true
+                    to gameInstance.owner.email
+                    subject "REMAR – O seu WAR \"${gameInstance.name}\" foi aprovado!"
+                    html '<h3>O seu WAR \"${gameInstance.name}\" foi aprovado! :)</h3> <br>'
+                }
+
+                render "success"
+            } else {
+                response.status = 500
+                render resp
+
+            }
+            // probably we don't need this anymore because when the WAR is deployed the bpmn is deployed too
+            // redirect controller: "process", action: "deploy", id: gameInstance.bpmn
         } else if (status == "reject" && gameInstance.status != "rejected") {
             gameInstance.status  = "rejected"
 
-            redirect controller: "process", action: "undeploy", id: gameInstance.bpmn
+            render "success"
 
         }
 
         gameInstance.save flush: true
 
-        render "ok"
     }
 
     @Transactional
-    def delete(Deploy deployInstance) {
-//
-//        if (gameInstance == null) {
-//            notFound()
-//            return
-//        }
-//
-//        if(gameInstance.developer == springSecurityService.currentUser){
-//            def war_dir = new File(grailsApplication.config.camundaWebapps+gameInstance.war_filename.minus(".war"))
-//            war_dir.mkdirs()
-//
-//            if(war_dir.exists()){
-//                println "Removendo deploy da aplicação..."
-//                if(war_dir.deleteDir()){
-//                    println "Remoção ocorrida com sucesso!"
-//                    println "Removendo arquivo .war..."
-//                    def war_file = new File(grailsApplication.config.camundaWebapps+gameInstance.war_filename)
-//                    if(war_file.delete()){
-//                        println "Arquivo .war removido com sucesso!"
-//                    }
-//                    else{
-//                        println "Problemas ao remover .war!"
-//                        redirect controller: "deploy", action: "index"
-//                    }
-//
-//                }
-//                else{
-//                    println "Problemas ao remover diretório!"
-//                    redirect controller: "deploy", action: "index"
-//                }
-//            }
-//
-//            gameInstance.delete flush:true
-//
-//            redirect controller: "deploy", action: "index"
-//        }
-//        else{
-//            flash.message = "Você não tem acesso a esse deploy"
-//            redirect action: 'index'
-//        }
+    def delete(Game gameInstance) {
+
+        if (gameInstance == null) {
+            response.status = 404
+            render "Not found"
+            return
+        }
+
+        if (gameInstance.owner == springSecurityService.currentUser) {
+            gameInstance.delete flush: true
+        }
+
+        render "success"
     }
 
     protected void notFound() {
