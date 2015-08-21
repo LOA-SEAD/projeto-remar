69249e084d102ea230a35300ccd0d0205485d03d
Matheus <matheus.frndes@gmail.com>
2015-08-21 13:56:47 -0300

    Missing class

---
+++ ProjetoREMAR/grails-app/domain/br/ufscar/sead/loa/remar/RequestMap.groovy
@@ -0,0 +1,20 @@
+package br.ufscar.sead.loa.remar
+
+import org.springframework.http.HttpMethod
+
+class RequestMap {
+
+	String url
+	String configAttribute
+	HttpMethod httpMethod
+
+	static mapping = {
+		cache true
+	}
+
+	static constraints = {
+		url blank: false, unique: 'httpMethod'
+		configAttribute blank: false
+		httpMethod nullable: true
+	}
+}
