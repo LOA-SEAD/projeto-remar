50414ff0cb12ce8e75c399db92aef3ca4caa9880
Matheus <matheus.frndes@gmail.com>
2015-09-11 01:49:58 -0300

    New security mapping config

--- EscolaMagica/grails-app/conf/BootStrap.groovy
+++ EscolaMagica/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
+import br.ufscar.sead.loa.remar.RequestMap
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        new RequestMap(url: '/question/**', configAttribute: 'IS_AUTHENTICATED_FULLY').save()
+        for (url in [
+                '/', '/index', '/index/info', '/doc/**', '/assets/**', '/**/js/**', '/**/css/**', '/**/images/**',
+                '/**/favicon.ico', '/data/**', '/**/scss/**', '/**/less/**', '/**/fonts/**', '/password/**',
+                '/moodle/**', '/exportedGame/**', '/static/**', '/login/**', '/logout/**', '/user/**',
+                '/facebook/**']) {
+            new RequestMap(url: url, configAttribute: 'permitAll').save()
+        }
     }
     def destroy = {
     }
