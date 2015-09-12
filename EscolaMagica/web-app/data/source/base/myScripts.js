f0bbebfbb8da447ae7ffff9af5992834dcfd1f08
Matheus <matheus.frndes@gmail.com>
2015-09-12 17:51:19 -0300

    Missing files from Suki's version (01edb6f)

---
+++ EscolaMagica/web-app/data/source/base/myScripts.js
@@ -0,0 +1,10 @@
+function send(enunciado,correta,resp0,resp1,resp2,resp3,escolhida)
+{
+console.log(enunciado);
+console.log(correta);
+console.log(resp0);
+console.log(resp1);
+console.log(resp2);
+console.log(resp3);
+console.log(escolhida);
+}
