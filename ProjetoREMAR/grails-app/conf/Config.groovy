dc72a08875ca15cc7fc1894fc2b93c486e2277df
Rener Baffa da Silva <renerbaffa@gmail.com>
2015-09-16 11:03:44 -0300

    Adicionando o arquivo de propriedades "moodle.properties"

--- ProjetoREMAR/grails-app/conf/Config.groovy
+++ ProjetoREMAR/grails-app/conf/Config.groovy
@@ -94,7 +94,7 @@ grails.hibernate.cache.queries = false
 grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
-
+grails.config.locations = ["classpath:moodle.properties"]
 
 
 
