cefe6430fb08d9e3629312c479410005cf71bba1
Matheus <matheus.frndes@gmail.com>
2015-11-22 14:21:28 -0200

    i18n configuration

--- remar/server/server.js
+++ remar/server/server.js
@@ -20,6 +20,13 @@ app.start = function() {
 /* Defining the views folder */
 app.set('view engine', 'jade');
 
+i18n.configure({
+  locales: ['en', 'pt-br'],
+  directory: "./locales"
+});
+
+app.use(i18n.init);
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
