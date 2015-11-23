0d0626cf9dc101fe1159c8993999762464745abd
Matheus <matheus.frndes@gmail.com>
2015-11-23 01:33:15 -0200

    Observer to always encrypt password

--- remar/common/models/user.js
+++ remar/common/models/user.js
@@ -1,7 +1,17 @@
+var bcrypt = require('bcrypt');
+
 module.exports = function(User) {
+  User.observe('before save', function (ctx, next) {
+    var instance = ctx.instance? ctx.instance : ctx.data;
 
-
-
-
-
+    if (instance.password) {
+      if (instance.password.length < 8) {
+        var err = new Error("Password is too short"); // TODO: figure out how to use loopback.ValidationError
+        err.statusCode = 422;
+        return next(err);
+      }
+      instance.password = bcrypt.hashSync(instance.password, 10);
+    }
+    next();
+  });
 };
