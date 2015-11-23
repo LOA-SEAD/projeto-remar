634b1edc1ddecdc1fbbad03eb5229957a3850f02
Matheus <matheus.frndes@gmail.com>
2015-11-23 01:39:15 -0200

    Update root password (temporary) & remove Denis user

--- remar/server/boot/db.js
+++ remar/server/boot/db.js
@@ -1,14 +1,11 @@
 module.exports = function(app) {
-
   app.dataSources.db.automigrate('user', function(err){
     if (err) throw err;
     var user = app.models.user;
 
     user.create([
       {username: 'root', email: 'root@remar.com.br',
-        firstName: 'ROOT',lastName: "ROOT", password: "senha", gender:"male"},
-      {username: 'deniscapp', email: 'denis_cappelini@hotmail.com',
-        firstName: 'Denis',lastName: "Cappelini", password: "senhadodenis", gender:"male"}
+        firstName: 'ROOT',lastName: "ROOT", password: "rootroot", gender:"male"}
     ], function(err, users) {
       if (err) throw err;
 
