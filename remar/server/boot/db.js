d619752bd47783281006d1cec61f4e4425b80807
Matheus <matheus.frndes@gmail.com>
2015-11-24 11:44:48 -0200

    Enhanced db boot script

--- remar/server/boot/db.js
+++ remar/server/boot/db.js
@@ -1,16 +1,33 @@
-module.exports = function(app) {
-  app.dataSources.db.automigrate('user', function(err){
-    if (err) throw err;
-    var user = app.models.user;
+module.exports = function (app) {
+  var db = app.dataSources.db;
+  var User = app.models.user;
+  var Resource = app.models.Resource;
+  var Platform = app.models.Platform;
+  var models = [User.modelName, Resource.modelName, Platform.modelName];
 
-    user.create([
-      {username: 'root', email: 'root@remar.com.br',
-        firstName: 'ROOT',lastName: "ROOT", password: "rootroot", gender:"male", dev: true}
-    ], function(err, users) {
-      if (err) throw err;
+  db.isActual(models, function (err, actual) {
+    if (!actual) {
+      console.log("Creating or updating tables");
+      db.autoupdate(models, function (err) {
+        if (err) throw err;
 
-      console.log('Users created:\n', users);
-    });
+        User.findOrCreate({username: 'root'}, {
+          username: 'root', email: 'contato@remar.dc.ufscar.br',
+          firstName: 'REMAR', lastName: 'Admin', password: 'rootroot', gender: 'other', dev: true
+        }, function (err, ignored) {
+          if (err) throw err;
 
+          console.log("root user: ok");
+        });
+
+        Platform.findOrCreate({name: 'web'}, {name: 'web'}, function (err, ignored) {
+          if (err) throw err;
+
+          console.log("Web platform: ok");
+        });
+      });
+    } else {
+      console.log("All models are up-to-date");
+    }
   });
 };
