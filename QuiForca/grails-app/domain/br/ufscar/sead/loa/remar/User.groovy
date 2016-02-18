45ba2e932a7d4336025c19e36000d75afd086d27
Matheus <matheus.frndes@gmail.com>
2016-02-18 14:13:35 -0200

    Fix: update user domain

--- QuiForca/grails-app/domain/br/ufscar/sead/loa/remar/User.groovy
+++ QuiForca/grails-app/domain/br/ufscar/sead/loa/remar/User.groovy
@@ -4,78 +4,79 @@ import org.springframework.security.core.GrantedAuthority
 
 class User {
 
-	transient springSecurityService
+    transient springSecurityService
 
-	String username
-	String password
-	boolean enabled = true
-	boolean accountExpired
-	boolean accountLocked
-	boolean passwordExpired
-	String email
-	String camunda_id
-	String firstName
-	String lastName
-	String facebookId
-	String moodleHash
-	String gender
+    String username
+    String password
+    boolean enabled
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
+    String email
+    String camunda_id
+    String firstName
+    String lastName
+    String facebookId
+    String moodleUsername
+    String gender
+    boolean firstAccess
 
 
-	static transients = ['springSecurityService']
+    static transients = ['springSecurityService']
 
-	static constraints = {
-		username blank: false, unique: true, nullable: false
-		password blank: false, nullable: false
-		firstName blank: false
-		lastName blank: true
-		email blank: false, email: true, unique: true
-		camunda_id nullable: true
-		facebookId nullable: true
-		moodleHash nullable: true
-		gender blank: false
+    static constraints = {
+        username blank: false, unique: true, nullable: false
+        password blank: false, nullable: false
+        firstName blank: false
+        lastName blank: true
+        email blank: false, email: true, unique: true
+        camunda_id nullable: true
+        facebookId nullable: true
+        moodleUsername nullable: true
+        gender blank: false
+        firstAccess blank: true, nullable: true
 
-	}
+    }
 
-	static mapping = {
-		password column: '`password`'
-		datasource 'remar'
-		tablePerHierarchy false
-	}
+    static mapping = {
+        password column: '`password`'
+        datasource 'remar'
+        tablePerHierarchy false
+    }
 
-	Set<Role> getAuthorities() {
-		UserRole.findAllByUser(this).collect { it.role }
-	}
+    Set<Role> getAuthorities() {
+        UserRole.findAllByUser(this).collect { it.role }
+    }
 
-	def beforeInsert() {
-		encodePassword()
-	}
+    def beforeInsert() {
+        encodePassword()
+    }
 
-	def beforeUpdate() {
-		if (isDirty('password')) {
-			encodePassword()
-		}
-	}
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
 
-	protected void encodePassword() {
-		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
-	}
+    protected void encodePassword() {
+        password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+    }
 
-	HashSet<GrantedAuthority> test() {
-		def roles = UserRole.findAllByUser(this).collect { it.role }
-		def auths = new HashSet<GrantedAuthority>()
-		roles.each { role ->
-			def auth = new GrantedAuthority() {
+    HashSet<GrantedAuthority> test() {
+        def roles = UserRole.findAllByUser(this).collect { it.role }
+        def auths = new HashSet<GrantedAuthority>()
+        roles.each { role ->
+            def auth = new GrantedAuthority() {
 
-				@Override
-				String getAuthority() {
-					return role.authority
-				}
-			}
-			auths.add(auth)
+                @Override
+                String getAuthority() {
+                    return role.authority
+                }
+            }
+            auths.add(auth)
 
-		} as Set<Role>
-		return auths
-	}
-
+        } as Set<Role>
+        return auths
+    }
 
 }
