b5b7eaf2265f52ca8db18175022338bd781305f2
LucasBocanegra <lucasfernandobocanegra@gmail.com>
2016-02-22 10:49:11 -0300

    create branch dev

---
+++ ProjetoREMAR/grails-app/domain/br/ufscar/sead/loa/remar/Category.groovy
@@ -0,0 +1,46 @@
+package br.ufscar.sead.loa.remar
+
+class Category {
+
+    static belongsTo = [owner: User]
+
+    static constraints = {
+        submittedAt blank: false
+        name blank: false
+        comment nullable: true, blank: true
+        bpmn nullable: true
+        moodle defaultValue: false
+        android defaultValue: false
+        web defaultValue: true
+        linux defaultValue: false
+        files nullable: true
+        width nullable: true
+        height nullable: true
+        description nullable: true, maxSize: 1000
+        pending nullable: true
+    }
+
+    String name
+    boolean active
+    int version
+    String uri
+
+    boolean web
+    boolean android
+    boolean linux
+    boolean moodle
+    String files
+
+    // deploy
+    Date submittedAt
+    String status
+    String comment
+    boolean valid
+    String bpmn
+
+    int width
+    int height
+
+    String description
+    boolean pending
+}
