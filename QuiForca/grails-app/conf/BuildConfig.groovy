63e3e36a2d214837a769e2cec3aecfa907ba57cf
Matheus <matheus.frndes@gmail.com>
2016-03-08 15:22:04 -0300

    Add missing MongoDB dependencies

--- QuiForca/grails-app/conf/BuildConfig.groovy
+++ QuiForca/grails-app/conf/BuildConfig.groovy
@@ -52,6 +52,8 @@ grails.project.dependency.resolution = {
         runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         // test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+        compile 'org.mongodb:mongodb-driver:3.2.1'
+        compile 'org.mongodb:bson:3.0.4'
     }
 
     plugins {
