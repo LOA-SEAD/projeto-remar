c40a2f9fa7c9814a8c79c658c8939a7e0386d518
Matheus <matheus.frndes@gmail.com>
2016-03-16 13:08:45 -0300

    The iframe now fits entirely on the page

--- ProjetoREMAR/web-app/js/frame.js
+++ ProjetoREMAR/web-app/js/frame.js
@@ -5,14 +5,7 @@
 window.addEventListener("load", function() {
     var frame = $("#frame");
 
-    if(!window.development) {
-        $(frame).load(function() {
-            $(this).height($(this).contents().find('html').height());
-        });
-    }
-});
-
-$(document).ready(function() {
-    var frame = $("#frame");
-    $(frame).height($(document).height() * 2);
+    $(frame).load(function() {
+        $(this).height($(this).contents().find('html').height());
+    });
 });
