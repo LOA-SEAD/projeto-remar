9b2ca2708a2461442fdacdfaebc82acea0695e44
Matheus <matheus.frndes@gmail.com>
2016-03-17 15:06:58 -0300

    Call iFrame Resizer

--- ProjetoREMAR/web-app/js/frame.js
+++ ProjetoREMAR/web-app/js/frame.js
@@ -3,9 +3,5 @@
  */
 
 window.addEventListener("load", function() {
-    var frame = $("#frame");
-
-    $(frame).load(function() {
-        $(this).height($(this).contents().find('html').height());
-    });
+    $('#frame').iFrameResize();
 });
