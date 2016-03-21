LOA-SEAD/projeto-remar

commit 6fe900fe6bc5229918bdc33f80eff7ce7ac72e68

Ajustes de interface

ProjetoREMAR/grails-app/views/user/show.gsp
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="layout" content="base">
+	<meta name="layout" content="materialize-layout">
 	<title>REMAR</title>
 </head>
 <body>

ProjetoREMAR/grails-app/views/resource/show.gsp
@@ -19,7 +19,7 @@
 							<input type="hidden" name="id" value="${resourceInstance.id}" id="hidden">
 							<div class="category">
 								<p> ${resourceInstance.category.name}</p>
-								<div class="stars">
+								<div class="stars hide-on-small-only">
 									<g:if test="${resourceInstance.sumUser == 0}">
 										<div id="rateYo-main" style="display: inline-block;"
 											 data-stars="0"></div>

ProjetoREMAR/grails-app/views/index/dashboard.gsp
@@ -74,7 +74,7 @@
                                         <div class="left">
                                             <g:if test="${gameInstance.sumUser == 0}">
                                                 <div class="rating-dashboard"
-                                                     data-stars="0}"></div>
+                                                     data-stars="0"></div>
                                             </g:if>
                                             <g:else>
                                                 <div class="rating-dashboard"
@@ -170,7 +170,7 @@
     </div>
 </div>
 
-<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
+%{--<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>--}%
 
 <g:javascript src="layout/dashboard.js"/>
 

ProjetoREMAR/grails-app/views/layouts/materialize-layout.gsp
@@ -16,6 +16,7 @@
                         <li>
                             <a href="#" data-activates="dropdown-user" class="dropdown-button">
                                 <img src="/data/users/${session.user.username}/profile-picture"
+                                <img src="/data/users/${session.user.username}/profile-picture"
                                      alt="${session.user.firstName}" class="circle profile-pic"
                                      data-beloworigin="true">
                             </a>
@@ -23,7 +24,7 @@
                         <ul id="dropdown-user" class="my-dropdown-menu collection">
                             <li class="collection-item left">
                                 <div class="row no-margin-bottom">
-                                    <div class="col s5 center">
+                                    <div class="col s8 center">
                                         <p class="title truncate">${session.user.firstName} ${session.user.lastName}</p>
                                         <p class="secondary-color truncate" title="${session.user.email}">${session.user.email}</p>
                                     </div>
@@ -35,12 +36,12 @@
                             </li>
                             <div class="row collection-item footer" style="margin-top: 115px;">
                                 <div class="col s6">
-                                    <a href="/my-profile" class="waves-effect btn-flat left">
+                                    <a href="/my-profile" class="waves-effect btn-flat left tooltipped" data-position="bottom" data-delay="50" data-tooltip="Perfil">
                                         <i class="material-icons">account_circle</i>
                                     </a>
                                 </div>
                                 <div class="col s6">
-                                    <a href="/logout/index" class="waves-effect btn-flat right">
+                                    <a href="/logout/index" class="waves-effect btn-flat right tooltipped" data-position="bottom" data-delay="50" data-tooltip="Sair">
                                         <i class="material-icons">power_settings_new</i>
                                         %{--Sair--}%
                                     </a>

ProjetoREMAR/grails-app/views/layouts/menu.gsp
@@ -10,7 +10,7 @@
     <div class="hide-on-large-only">
         <div class="row no-margin-bottom valign-wrapper">
             <div class="col s4">
-                <img src="../data/users/${session.user.username}/profile-picture"
+                <img src="/data/users/${session.user.username}/profile-picture"
                      alt="${session.user.firstName}" class="circle responsive-img"
                      data-beloworigin="true">
             </div>

ProjetoREMAR/grails-app/views/resource/_form.gsp
@@ -14,7 +14,7 @@
 			<div class="input-field col s12">
 				<i class="material-icons suffix green-text active">done</i>
 				<textarea id="description" class="materialize-textarea" length="250" name="description" required ></textarea>
-				<label for="description">Textarea</label>
+				<label for="description">Descrição</label>
 				<span id="desc-error" class="invalid-textarea" style="left: 0.75rem">Este campo não pode ser vazio!</span>
 			</div>
 		</div>

ProjetoREMAR/web-app/css/style.css
@@ -623,7 +623,7 @@ footer.page-footer{
 }
 
 .card.show-personalize > .card-content button{
-    margin-top: -42px;
+    margin-top: -35px;
 }
 
 .card.show-personalize .image img{

commit de1a601fce70268fd5f19ec73f6281fe959aebb3

Change layout back to the correct one
Fix issue caused by 6fe900fe6bc5229918bdc33f80eff7ce7ac72e68

ProjetoREMAR/grails-app/views/user/show.gsp
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="layout" content="materialize-layout">
+	<meta name="layout" content="base">
 	<title>REMAR</title>
 </head>
 <body>