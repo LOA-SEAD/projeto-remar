040eef67c09d05a53db641d839d8299b6b42a7bf
LucasBocanegra <lucasfernandobocanegra@gmail.com>
2016-04-13 08:49:02 -0300

    problema nos comentários resolvido

--- ProjetoREMAR/grails-app/conf/BootStrap.groovy
+++ ProjetoREMAR/grails-app/conf/BootStrap.groovy
@@ -98,7 +98,8 @@ class BootStrap {
 
             for (url in [
                     '/dashboard', '/process/**', '/developer/new', '/exported-resource/**', '/exportedResource/**', '/frame/**', '/my-profile',
-                    '/user/update', '/resource/customizableGames','/resource/show/**', '/moodle/link/**', '/moodle/unlink/**', '/resource/saveRating/**'
+                    '/user/update', '/resource/customizableGames','/resource/show/**', '/moodle/link/**', '/moodle/unlink/**', '/resource/saveRating/**',
+                    '/resource/updateRating/**',  '/resource/deleteRating/**'
             ]) {
                 new RequestMap(url: url, configAttribute: 'IS_AUTHENTICATED_FULLY').save()
             }
