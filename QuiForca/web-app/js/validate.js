41672bc64b64e825d6b5a7ae0369dc06060cd8d6
marcus <marcus-pf@hotmail.com>
2016-05-05 17:42:15 -0300

    Validação do campo de resposta

--- QuiForca/web-app/js/validate.js
+++ QuiForca/web-app/js/validate.js
@@ -1,9 +1,15 @@
 function validate(evt) {
     var theEvent = evt || window.event;
     var key = theEvent.keyCode || theEvent.which;
+    if(key == 37 || key == 38 || key == 39 || key == 40 || key == 8) { // Left / Up / Right / Down Arrow, Backspace, Delete keys
+        return;
+    }
     key = String.fromCharCode( key );
-    var regex = /[a-z A-Z]|\./;
-    if( !regex.test(key) ) {
+
+   // var regex = /[a-z|A-Z|/]/;
+    var regex = /[a-zA-Z]/;
+    console.log("key: " + key);
+    if( !regex.test(key)) {
         theEvent.returnValue = false;
         if(theEvent.preventDefault) theEvent.preventDefault();
     }
