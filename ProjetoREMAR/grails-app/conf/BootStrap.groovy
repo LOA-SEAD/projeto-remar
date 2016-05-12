9c365ccfd623192ebb8252c707595e2e3ac01f82
Matheus <matheus.frndes@gmail.com>
2016-05-12 17:50:00 -0300

    Simplified and optimized db population at Bootstrap

--- ProjetoREMAR/grails-app/conf/BootStrap.groovy
+++ ProjetoREMAR/grails-app/conf/BootStrap.groovy
@@ -34,20 +34,18 @@ class BootStrap {
             def admin = new User(
                     username: "admin",
                     password: grailsApplication.config.users.password,
-                    email: "admin@gmail.com",
+                    email: "admin@remar.dc.ufscar.br",
                     firstName: "Admin",
                     lastName: "User",
-                    gender: 'male',
                     enabled: true
             )
 
             def guest = new User(
                     username: "guest",
                     password: grailsApplication.config.users.password,
-                    email: "guest@gmail.com",
+                    email: "admin@remar.dc.ufscar.br",
                     firstName: "Guest",
                     lastName: "User",
-                    gender: 'female',
                     enabled: true,
             )
 
@@ -60,80 +58,53 @@ class BootStrap {
             log.debug "Users: ok"
         }
 
-        def platforms = Platform.findAll();
+        Platform.findOrSaveByName('Android')
+        Platform.findOrSaveByName('Linux')
+        Platform.findOrSaveByName('Web')
+        Platform.findOrSaveByName('Moodle')
 
-        if (!platforms) {
-            new Platform(name: "Android").save flush: true
-            new Platform(name: "Linux").save flush: true
-            new Platform(name: "Web").save flush: true
-            new Platform(name: "Moodle").save flush: true
+        Category.findOrSaveByName("Ação/Aventura")
+        Category.findOrSaveByName("Estratégia")
+        Category.findOrSaveByName("Saúde")
+        Category.findOrSaveByName("RPG")
+        Category.findOrSaveByName("Esporte")
+        Category.findOrSaveByName("Simulação")
+        Category.findOrSaveByName("Puzzle")
+        Category.findOrSaveByName("Clássicos")
 
-            log.debug "Platforms: ok"
+        for (url in [
+                '/dashboard', '/process/**', '/developer/new', '/exported-resource/**', '/exportedResource/**', '/frame/**', '/my-profile',
+                '/user/update', '/resource/customizableGames', '/resource/show/**', '/moodle/link/**', '/moodle/unlink/**', '/resource/saveRating/**',
+                '/resource/updateRating/**', '/resource/deleteRating/**'
+        ]) {
+            RequestMap.findOrSaveByUrlAndConfigAttribute(url, 'IS_AUTHENTICATED_FULLY')
         }
 
-        def category = Category.findAll();
-
-        if(!category){
-            new Category(name: "Ação/Aventura",description:  "jogos de ação e aventura").save flush: true
-            new Category(name: "Estratégia", description: "jogos de estratégia").save flush: true
-            new Category(name: "Saúde", description: "jogos da area de saúde").save flush: true
-            new Category(name: "RPG", description: "jogos de RPG").save flush: true
-            new Category(name: "Esporte", description: "jogos de esporte").save flush: true
-            new Category(name: "Simulação", description: "jogos de simulação").save flush: true
-            new Category(name: "Puzzle", description: "jogos de puzzle").save flush: true
-            new Category(name: "Clássicos", description: "jogos clássicos").save flush: true
-
-
-            log.debug "Category: ok"
+        for (url in [
+                '/', '/index', '/index/project', '/index/info', '/doc/**', '/assets/**',
+                '/exportedResource/publicGames', '/exported-resource/searchGame', '/**/js/**', '/**/css/**',
+                '/**/images/**', '/**/favicon.ico', '/data/**', '/**/scss/**', '/**/less/**', '/**/fonts/**',
+                '/**/font/**', '/password/**', '/moodle/**', '/exportedGame/**', '/static/**', '/login/**',
+                '/logout/**', '/signup/**', '/user/**', '/facebook/**', '/published/**'
+        ]) {
+            RequestMap.findOrSaveByUrlAndConfigAttribute(url, 'permitAll')
         }
 
-        if (Environment.current == Environment.DEVELOPMENT) {
-            RequestMap.findAll().each { it.delete flush: true }
+        for (url in [
+                '/process/deploy', '/process/undeploy'
+        ]) {
+            RequestMap.findOrSaveByUrlAndConfigAttribute(url, 'ROLE_ADMIN')
         }
 
-        if (!RequestMap.list()) { // will be true if env == dev or if first run in production
-            for (url in [
-                    '/dashboard', '/process/**', '/developer/new', '/exported-resource/**', '/exportedResource/**', '/frame/**', '/my-profile',
-                    '/user/update', '/resource/customizableGames','/resource/show/**', '/moodle/link/**', '/moodle/unlink/**', '/resource/saveRating/**',
-                    '/resource/updateRating/**',  '/resource/deleteRating/**'
-            ]) {
-                new RequestMap(url: url, configAttribute: 'IS_AUTHENTICATED_FULLY').save()
-            }
-
-            for (url in [
-                    '/', '/index', '/index/project' , '/index/info', '/doc/**', '/assets/**', '/exportedResource/publicGames' , '/exported-resource/searchGame', '/**/js/**', '/**/css/**', '/**/images/**',
-                    '/**/favicon.ico', '/data/**', '/**/scss/**', '/**/less/**', '/**/fonts/**', '/**/font/**',
-                    '/password/**', '/moodle/**', '/exportedGame/**', '/static/**', '/login/**', '/logout/**', '/signup/**', '/user/**',
-                    '/facebook/**', '/published/**']) {
-                new RequestMap(url: url, configAttribute: 'permitAll').save()
-            }
-
-
-
-        new RequestMap(url: '/resource/**', configAttribute: 'ROLE_DEV').save()
-        new RequestMap(url: '/resource/edit', configAttribute: 'ROLE_ADMIN').save()
-        new RequestMap(url: '/resource/review', configAttribute: 'ROLE_ADMIN').save()
-        new RequestMap(url: '/process/deploy', configAttribute: 'ROLE_ADMIN').save()
-        new RequestMap(url: '/process/undeploy', configAttribute: 'ROLE_ADMIN').save()
-        new RequestMap(url: '/user/index', configAttribute: 'ROLE_ADMIN').save()
-        new RequestMap(url: '/reset', configAttribute: 'ROLE_ADMIN').save()
-            for (url in [
-                    '/resource/edit', '/resource/review', '/process/deploy', '/process/undeploy',
-            ]) {
-                new RequestMap(url: url, configAttribute: 'ROLE_ADMIN').save()
-            }
-
-            for (url in [
-                    '/resource/**'
-            ]) {
-                new RequestMap(url: url, configAttribute: 'ROLE_DEV').save()
-            }
-
-            log.debug "RequestMaps: ok"
+        for (url in [
+                '/resource/**'
+        ]) {
+            RequestMap.findOrSaveByUrlAndConfigAttribute(url, 'ROLE_DEV')
         }
 
         log.debug "Bootstrap: done"
     }
+
     def destroy = {
     }
 }
