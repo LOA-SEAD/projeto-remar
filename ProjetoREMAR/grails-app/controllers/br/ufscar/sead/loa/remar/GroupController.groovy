d71f3e86b92e922beb36ef56e785f30736f30f1a
deniscapp <denis_cappelini@hotmail.com>
2016-05-18 13:20:27 -0300

    create() in GroupController

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GroupController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GroupController.groovy
@@ -3,9 +3,30 @@ package br.ufscar.sead.loa.remar
 
 class GroupController {
 
+    def springSecurityService
+
     def list() {
         def groups = Group.findAll()
 
         render(view: "list", model: [groups: groups])
     }
+
+    def create(){
+        println params
+        def groupInstance = new Group()
+        ArrayList<User> owners = new ArrayList<>()
+
+        owners.add(session.user)
+        groupInstance.name = params.groupname
+        groupInstance.privacy = params.privacy
+
+        groupInstance.owners = owners
+
+        groupInstance.save flush: true
+
+
+    }
+
+
+
 }
