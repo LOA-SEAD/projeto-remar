1faccda9be86ca05016d066060828a000e64e56c
deniscapp <denis_cappelini@hotmail.com>
2016-05-25 20:25:44 -0300

    'delete' action removing admins, 'makeAdmin' action and 'show' and 'list' fix

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GroupController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GroupController.groovy
@@ -6,7 +6,7 @@ class GroupController {
     def springSecurityService
 
     def list() {
-        def groups = Group.findAll()
+        def groups = Group.findAllByOwner(session.user)
 
         render(view: "list", model: [groups: groups])
     }
@@ -15,7 +15,7 @@ class GroupController {
         println params
         def groupInstance = new Group()
 
-        groupInstance.addToOwners(session.user)
+        groupInstance.owner = session.user
         groupInstance.name = params.groupname
         groupInstance.privacy = params.privacy
 
@@ -25,32 +25,63 @@ class GroupController {
 
     def show(){
         def group = Group.findById(params.id)
-        def groupUsers = UserGroup.findAllByGroup(group)
-
+        def admins = group.getAdmins().toList()
+//        def groupUsers = UserGroup.findAllByGroup(group)
+        def groupUsers = group.getUserGroups()
         render(view: "show", model: [group: group, groupUsers: groupUsers])
     }
 
     def addUser(){
-        println params
 
         def userGroup = new UserGroup()
         userGroup.group = Group.findById(params.groupid)
         userGroup.user = User.findById(params.userid)
-
+        //TODO filtrar usuarios ja adicionados
         userGroup.save flush: true
 
         redirect(action: "show", id: userGroup.groupId)
     }
 
+    def makeAdmin(){
+        def user = User.findById(params.id)
+        def userGroup = UserGroup.findByUser(user)
+        def group = userGroup.group
+
+        group.addToAdmins(user)
+        group.save flush: true
+
+        redirect(action: "show", id: userGroup.groupId)
+
+    }
+
+    def removeAdmin(){
+
+        def user = User.findById(params.id)
+        def userGroup = UserGroup.findByUser(user)
+        def group = userGroup.group
+
+        group.removeFromAdmins(user)
+        group.save flush: true
+
+        redirect(action: "show", id: userGroup.groupId)
+
+    }
+
     def delete(){
         def userGroup = UserGroup.findByUser(User.findById(params.id))
-        def groupId = userGroup.group.id
+        def adminList = userGroup.group.admins.toList()
 
         if(userGroup){
+            if(adminList.contains(userGroup.user)){
+                def group = userGroup.group
+                group.removeFromAdmins(userGroup.user)
+                group.save flush: true
+            }
+
             userGroup.delete flush: true
-            redirect(action: "show", id: groupId, message: true )
+            redirect(action: "show", id: userGroup.group.id, message: true )
         }else{
-            redirect(action: "show", id: groupId , message: false )
+            redirect(action: "show", id: userGroup.group.id , message: false )
         }
 
 
