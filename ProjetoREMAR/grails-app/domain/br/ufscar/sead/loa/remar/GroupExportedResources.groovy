f97fa439a7432b433537f0a25bd17b8df15e63c5
deniscapp <denis_cappelini@hotmail.com>
2016-05-30 23:52:13 -0300

    GroupExportedResources model

--- ProjetoREMAR/grails-app/domain/br/ufscar/sead/loa/remar/GroupExportedResources.groovy
+++ ProjetoREMAR/grails-app/domain/br/ufscar/sead/loa/remar/GroupExportedResources.groovy
@@ -2,7 +2,7 @@ package br.ufscar.sead.loa.remar
 
 class GroupExportedResources {
 
-    static belongsTo = [group: Group, exportedResources: ExportedResource]
+    static belongsTo = [group: Group, exportedResource: ExportedResource]
 
     static constraints = {
     }
