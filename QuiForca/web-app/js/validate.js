b74241c755e03e2fb9e6d1a0ae9ff6900ad8b611
marcus <marcus-pf@hotmail.com>
2016-06-02 09:57:29 -0300

    Permitindo espaços em branco na respota

--- QuiForca/web-app/js/validate.js
+++ QuiForca/web-app/js/validate.js
@@ -1,7 +1,7 @@
 function validate(evt) {
     var theEvent = evt || window.event;
     var key = theEvent.keyCode || theEvent.which;
-    if(key == 37 || key == 38 || key == 39 || key == 40 || key == 8) { // Left / Up / Right / Down Arrow, Backspace, Delete keys, blank code is 32
+    if(key == 37 || key == 38 || key == 39 || key == 40 || key == 8 || key==32) { // Left / Up / Right / Down Arrow, Backspace, Delete keys, blank code is 32
         return;
     }
     key = String.fromCharCode( key );
