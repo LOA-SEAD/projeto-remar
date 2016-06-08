4be491fd46d22b984a74d978a43821c226a24c24
deniscapp <denis_cappelini@hotmail.com>
2016-06-08 15:47:37 -0300

    "delete" action for groupExportedResource

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GroupExportedResourcesController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/GroupExportedResourcesController.groovy
@@ -4,18 +4,30 @@ class GroupExportedResourcesController {
 
     def index() {}
 
-    def deleteall(){
-        GroupExportedResources.deleteAll()
+    def delete(){
+        def groupExportedResource = GroupExportedResources.findById(params.id)
+        println groupExportedResource
+        def group = groupExportedResource.group
+        println group
+        if(session.user.id == group.owner.id) {
+            println "entrou"
+            groupExportedResource.delete flush: true
+            redirect(action: "show", id: group.id,  controller: "group")
+        }else{
+            render(view: "../401", status: 401)
+        }
     }
 
     def addGroupExportedResources(){
-
+        println params.exportedresource
+        println params.groupsid
         def exportedResource = ExportedResource.findById(params.exportedresource)
         params.groupsid.each{
             def groupExportedResource = new GroupExportedResources()
 
             groupExportedResource.group = Group.findById("${it}")
             groupExportedResource.exportedResource = exportedResource
+
             println groupExportedResource.group
             println groupExportedResource.exportedResource
 
