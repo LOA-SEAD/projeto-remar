d24af38e9e130e97c76eb79ada480f8e647f8571
marcus <marcus-pf@hotmail.com>
2016-07-07 15:44:21 -0300

    Corrigindo erro de gramática

--- ProjetoREMAR/web-app/js/licenseShow.js
+++ ProjetoREMAR/web-app/js/licenseShow.js
@@ -13,7 +13,7 @@ $(function () {
         $("#licenseInfo").append(" <div class='row center-align'> <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>" +
             " <img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/88x31.png' /> " +
             " </a>");
-        $("#licenseInfo").append("Esta obra está licenciado com uma Licença <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Atribuição-CompartilhaIgual 4.0 Internacional</a>.</div>");
+        $("#licenseInfo").append("Esta obra está licenciada com uma Licença <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Atribuição-CompartilhaIgual 4.0 Internacional</a>.</div>");
 
     }
     else{
@@ -22,7 +22,7 @@ $(function () {
             $("#licenseInfo").append(" <div class='row center-align'> <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'> " +
                 "<img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png' />" +
                 "</a> ");
-            $("#licenseInfo").append("Esta obra está licenciado com uma Licença <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>Creative Commons Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</div>");
+            $("#licenseInfo").append("Esta obra está licenciada com uma Licença <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>Creative Commons Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</div>");
 
         }
     }
