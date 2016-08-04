695f6748cceb4a6ebd55018901e2ed1a4139581f
marcus <marcus-pf@hotmail.com>
2016-08-04 09:58:39 -0300

    Making validations when input change

--- ProjetoREMAR/web-app/js/dspace/validateSubmit.js
+++ ProjetoREMAR/web-app/js/dspace/validateSubmit.js
@@ -1,3 +1,7 @@
+var author = $("#author");
+var editor = $("#editor");
+var citation = $("#citation");
+var description = $("#description");
 function validateSubmit(){
     var error = 0;
     if($("#title").val()==null || $("#title").val() == ""){
@@ -21,6 +25,39 @@ function validateSubmit(){
         $("#nextButton").addClass('hide');
     }
 }
+function validateAuthor() {
+    if( author.val()==null || author.val() == ""){
+        author.removeClass('valid').addClass('invalid');
+    }
+    else{
+        author.removeClass('invalid').addClass('valid');
+    }
+}
+function validateEditor() {
+    if( editor.val()==null || editor.val() == ""){
+        editor.removeClass('valid').addClass('invalid');
+    }
+    else{
+        editor.removeClass('invalid').addClass('valid');
+    }
+}
+function validadteCitation(){
+    if( citation.val()==null || citation.val() == ""){
+        citation.removeClass('valid').addClass('invalid');
+    }
+    else{
+        citation.removeClass('invalid').addClass('valid');
+    }
+}
+function validateDescription(){
+    if( description.val()==null || description.val() == ""){
+        description.removeClass('valid').addClass('invalid');
+    }
+    else{
+        description.removeClass('invalid').addClass('valid');
+    }
+}
+
 
 $( document ).ready(function() {
 
@@ -28,6 +65,44 @@ $( document ).ready(function() {
     validateSubmit();
     $("#title").change(function () {
         validateSubmit();
-    })
+        validateAuthor();
+        validateEditor();
+        validadteCitation();
+        validateDescription();
+    });
+
+    author.change(function () {
+        validateSubmit();
+        validateAuthor();
+        validateEditor();
+        validadteCitation();
+        validateDescription();
+    });
+
+    editor.change(function () {
+        validateSubmit();
+        validateAuthor();
+        validateEditor();
+        validadteCitation();
+        validateDescription();
+    });
+
+    citation.change(function () {
+        validateSubmit();
+        validateAuthor();
+        validateEditor();
+        validadteCitation();
+        validateDescription();
+    });
+
+    description.change(function () {
+        validateSubmit();
+        validateAuthor();
+        validateEditor();
+        validadteCitation();
+        validateDescription();
+    });
+
+
 });
 
