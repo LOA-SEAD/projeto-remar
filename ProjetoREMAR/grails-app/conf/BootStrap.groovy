fa48f4d0d27545daf2c362cb864e60bd0ad54c7e
deniscapp <denis_cappelini@hotmail.com>
2016-08-20 01:12:03 -0300

    fix groups permission

--- ProjetoREMAR/grails-app/conf/BootStrap.groovy
+++ ProjetoREMAR/grails-app/conf/BootStrap.groovy
@@ -68,7 +68,7 @@ class BootStrap {
         for (url in [
                 '/dashboard', '/process/**', '/developer/new', '/exported-resource/**', '/exportedResource/**', '/my-profile',
                 '/user/update', '/resource/customizableGames', '/resource/show/**', '/moodle/link/**', '/moodle/unlink/**', '/resource/saveRating/**',
-                '/resource/updateRating/**', '/resource/deleteRating/**','/group','/group/user-stats/**','/group/stats/**','/user-group/**','/group-exported-resources/**',
+                '/resource/updateRating/**', '/resource/deleteRating/**','/group/**','/group/user-stats/**','/group/stats/**','/user-group/**','/group-exported-resources/**',
                 '/dspace/**'
         ]) {
             RequestMap.findOrSaveByUrlAndConfigAttribute(url, 'isAuthenticated()')
