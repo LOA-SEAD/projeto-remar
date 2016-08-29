507ad19c7454c144fb56e4d2bca0d70fa430a25a
Matheus Fernandes <github@matheus.top>
2016-08-29 11:57:49 -0300

    Remove env.sh in favor of fixed paths on build.sh

--- ProjetoREMAR/web-app/scripts/crosswalk/build.sh
+++ ProjetoREMAR/web-app/scripts/crosswalk/build.sh
@@ -4,7 +4,7 @@
 # $2: Game uri
 # $3: Game name
 
-source $1/scripts/crosswalk/env.sh
+export ANDROID_HOME="/dev-tools/android"
 
 cd $1/data/resources/sources/$2/base
 
@@ -16,7 +16,7 @@ cp $1/stats/logo-remar-preto-transparente.png .
 
 sed -i.bkp "s/NAME/$3/" manifest.json
 
-make_apk.py --package br.ufscar.sead.loa.remar.published.$2 --manifest manifest.json --target-dir ..
+/dev-tools/crosswalk/make_apk.py --package br.ufscar.sead.loa.remar.published.$2 --manifest manifest.json --target-dir ..
 
 #rm manifest.json manifest.json.bkp .REMAR login.js login.html logo-remar-preto-transparente.png
 
