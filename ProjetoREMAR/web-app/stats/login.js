916f0f51bb07b6479296ff2c1c0f97f833850ad5
deniscapp <denis_cappelini@hotmail.com>
2016-09-21 09:50:17 -0300

    checking if user is online before request

--- ProjetoREMAR/web-app/stats/login.js
+++ ProjetoREMAR/web-app/stats/login.js
@@ -1,24 +1,29 @@
 	function submitLogin(_this){
-		var choice = $(_this).attr('data-choice');
-		$.ajax({
+		var online = navigator.onLine;
+		if(online) {
+			var choice = $(_this).attr('data-choice');
+			$.ajax({
 				type: 'POST',
 				url: 'http://remar.dc.ufscar.br/group/isLogged', //TODO always change to remar.dc.ufscar.br later
 				data: {
 					username: $('#username').val(),
 					password: $('#password').val(),
-					choice: choice? choice : "null"
+					choice: choice ? choice : "null"
 				},
-				success: function(data){
+				success: function (data) {
 					$('#check-login').remove();
 				}, statusCode: {
-					200: function (response){
+					200: function (response) {
 						console.log("200 ok");
 						window.location.href = "./index.html"
 					},
-					401: function (response){
-						$('body').html(response.responseText);
+					401: function (response) {
+						$('body').append(response.responseText);
 					}
 				}
 			});
+		} else{
+			window.location.href = "./index.html"
+		}
 	}
 		
