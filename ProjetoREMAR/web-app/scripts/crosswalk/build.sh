e57c545cd6726d66d718d35d66a9fa400f6bc36d
Delano Beder <delanobeder@yahoo.com.br>
2016-09-22 07:55:25 -0300

    Changes - scripts build.sh update.sh

--- ProjetoREMAR/web-app/scripts/crosswalk/build.sh
+++ ProjetoREMAR/web-app/scripts/crosswalk/build.sh
@@ -2,10 +2,9 @@
 
 # $1: Application root path (where /data, /scripts, /data etc are located)
 # $2: Game uri
-# $3: Game name
 
 export ANDROID_HOME="/dev-tools/android"
-
+export CROSSWALK_PATH="/dev-tools/crosswalk"
 export PATH=$ANDROID_HOME:$PATH
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
@@ -18,17 +17,14 @@ cp $1/stats/login.js .
 cp $1/stats/login.html .
 cp $1/stats/logo-remar-preto-transparente.png .
 
-sed -i.bkp "s/NAME/$3/" manifest.json
+sed -i.bkp "s/NAME/$2/" manifest.json
 
-/dev-tools/crosswalk/make_apk.py --package br.ufscar.sead.loa.remar.published.$2 --manifest manifest.json --target-dir ..
-
-#rm manifest.json manifest.json.bkp .REMAR login.js login.html logo-remar-preto-transparente.png
+${CROSSWALK_PATH}/make_apk.py --package br.ufscar.sead.loa.remar.published.$2 --manifest manifest.json --target-dir ..
 
 cd ..
 
 mkdir android
 mv *.apk android
-
 
 for i in $(find . -name "*.apk")
 do
