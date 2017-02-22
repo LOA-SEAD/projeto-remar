c00aefb88a40069ee289be668bc22f35ee663640
Pedro Garcia <garcia.ph@gmail.com>
2017-02-22 11:03:21 -0300

    removing trash

--- LabTecA/target/work/plugins/asset-pipeline-2.1.5/META-INF/assets/asset-pipeline/test/absolute-path/test/test_full_tree.js
+++
@@ -1,2 +0,0 @@
-//= require_full_tree /asset-pipeline/test/absolute-path/full-tree
-//= require_full_tree asset-pipeline/test/absolute-path/not-included
--- LabTecA/target/work/plugins/asset-pipeline-2.1.5/META-INF/assets/asset-pipeline/test/test_multiple_file_directive.js
+++
@@ -1,3 +0,0 @@
-//=require asset-pipeline/test/libs/file_a,asset-pipeline/test/libs/subset/subset_a,asset-pipeline/test/libs/file_b
-
-console.log("Initial Testing Stack for dependency load order");
