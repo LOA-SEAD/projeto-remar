7f2f5a5a55e1bcf47e96b4fed7543bb079d242dc
garcia-pedro-hr <garcia.ph@gmail.com>
2017-04-24 17:45:20 -0300

    Update Página Modelo

--- Standardization/js/standardization.js
+++ Standardization/js/standardization.js
@@ -1,13 +1,39 @@
-
-window.onload = function() {
-  $('.modal').modal();
-  $('.grid').masonry({
-    itemSelector: '.grid-item'
-  });
-}
+// Created by Pedro Garcia
+// 04-12-2017
 
 $(document).ready(function() {
-  $('.grid').masonry('layout');
+  console.log('document ready');
+
+  // Materialize Stuff
+  var $modal = $('.modal').modal();
+
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Cores": null,
+      "Botões": null,
+      "Modal": null,
+      "Formulário": null
+    },
+    limit: 20,
+    onAutocomplete: function(val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1
+  });
+
+  console.log('materialize loaded');
+
+  // Sticky Kit
+  $('.navbox-container').stick_in_parent();
+  console.log('Sticky Kit loaded');
+
+  // Smooth Scrolling
+  $('.nav-btn a').on('click', function() {
+    var scrollto = $($(this).attr('href')).top - $navbarHeight;
+    $('html body').animate({
+      scrollTop: scrollto
+    }, 800);
+  });
 });
 
 // Normaliza espaços em branco de códigos mostrados dentro de blocos PrismJS
