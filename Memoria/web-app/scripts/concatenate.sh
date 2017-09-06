f437543022ee592b12114828f80f861a865fb04b
Hugo Barros <hugomrbr@hotmail.com>
2017-09-06 11:09:25 -0300

    Now we put the uploaded image inside a border
    - Resizes the image into a smaller size, so we put the uploaded image inside our border file
    - Few more comments

--- Memoria/web-app/scripts/concatenate.sh
+++ Memoria/web-app/scripts/concatenate.sh
@@ -1,5 +1,8 @@
  #! /bin/bash
 
+# how to call:
+# ./concatenate.sh [-v -h] 1 9 2
+
 #$1 = -p or -l
 #$2 = destino (facil, medio, dificil)
 #$3 = tiles_path
@@ -12,19 +15,18 @@ the options are:
     -v  vertical
     -h  horizontal
 "
-while getopts ':v:h:' option; do
+while getopts ':vh' option; do
   case "$option" in
     #h) echo "$usage" was a help command before;
     #   exit
     #   ;;
-    v) WIDTH=80
-       HEIGHT=120
-       texture_file="$3/../../../images/back.png"
+    v) WIDTH=68
+       HEIGHT=108
+       border_file="$3/../../../images/back.png"
        ;;
-    h) WIDTH=160
-       HEIGHT=120
-       # TODO change this to a texture image that is 160x120
-       texture_file="$3/../../../images/back.png"
+    h) WIDTH=148
+       HEIGHT=108
+       border_file="$3/../../../images/carta_virada.png"
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -36,35 +38,39 @@ while getopts ':v:h:' option; do
        ;;
   esac
 done
-# how to call:
-# ./script.sh -p 1 9 2 
 
 TILES_PATH="$3/" # should be something like [..]/data/1/tiles/
 
-b=$(echo $@ | tr " " "\n")
+b=$(echo $@ | tr " " "\n") # this is a list created from the params string separated by space
+
 tile_file_names=""
 dot="."
 n=0
 
-# this for searches in all the params this script receives for strings that contains a dot
+# this FOR searches for strings that contains a dot through all the params received (searches the b list)
 # those are the image names to be put in the concatenate param
 for i in $b
 do
 	if test "${i#*$dot}" != "$i"
 	then
+      convert $TILES_PATH$i -resize "${WIDTH}x${HEIGHT}!" $TILES_PATH$i #resizes the image to the standard size we need
+      composite -gravity center $TILES_PATH$i $border_file $TILES_PATH$i #puts the uploaded image inside our borders
 	  tile_file_names="$tile_file_names$TILES_PATH$i " #this will be generating a big string: img filenames separated by space
       n=$((n+1))
 	fi
 done
 
+# the var "n" was representing the 'number of images'
+# now it will be 'number of pairs'
 n=$((n/2))
 
-echo "${WIDTH}x${HEIGHT}"
+#$2 = destino (facil, medio, dificil) , will create a dir with the specified parameter
 mkdir -p $TILES_PATH$2
 destino="$TILES_PATH$2/cartas_difficulty.png"
-montage -mode concatenate -tile ${n}x2 -texture $texture_file -geometry "${WIDTH}x${HEIGHT}!" ${tile_file_names} $destino
 
-rm $tile_file_names
+montage -mode concatenate -tile ${n}x2 ${tile_file_names} $destino
 
 # water mark:::
 # composite -dissolve 75% -gravity south wmark_image.png capa.png resultado_wm.png
+
+rm $tile_file_names
