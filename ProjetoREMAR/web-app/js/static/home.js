11fb4dac4d02588310cca6cf8e7dc34bbf075989
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2017-10-23 22:50:59 -0200

    Static pages (remar project info)

---
+++ ProjetoREMAR/web-app/js/static/home.js
@@ -0,0 +1,7 @@
+// O atributo "href" de um .modal-trigger deve especificar o ID do modal ao qual está ligado
+$(document).ready(function() {
+    $('.modal').modal();
+    $('.slider').slider({
+    	 height: "350px"
+    });
+});
