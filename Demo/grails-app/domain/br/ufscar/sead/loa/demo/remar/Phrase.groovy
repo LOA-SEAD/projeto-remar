b55ad159725b7d4fe26952f2fc00cf65ae00ad9f
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2017-11-16 18:44:49 -0200

    Update domain class

--- Demo/grails-app/domain/br/ufscar/sead/loa/demo/remar/Phrase.groovy
+++ Demo/grails-app/domain/br/ufscar/sead/loa/demo/remar/Phrase.groovy
@@ -3,12 +3,14 @@ package br.ufscar.sead.loa.demo.remar
 class Phrase {
 
     String content
-    
+    String author
     long ownerId
     String taskId
 
+
     static constraints = {
         content blank: false, maxSize: 150
+        author blank: false
         ownerId blank: false, nullable: false
         taskId nullable: true
     }
