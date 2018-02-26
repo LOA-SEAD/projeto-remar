31ca4804ea02ec257e7ee1c9eb4fb5692e3472f8
Lucas Yuji S. Aciole <lucas.aciole@gmail.com>
2018-02-26 12:19:56 -0300

    Change verification to use regex instead of fixed string

--- ProjetoREMAR/web-app/js/remar/admin/admin.users.js
+++ ProjetoREMAR/web-app/js/remar/admin/admin.users.js
@@ -45,10 +45,10 @@ $(document).ready(function() {
         var id = $row.data('user-id');
 
         // Show warning box for action confirmation
-        if ($row.children('.user-status').html() == "Ativo") {
-            $('#warning-box-message').html(' <span id="warning-user">' + GMS.DISABLE_USER_WARNING_MSG + name + '</span> ?');
+        if ($row.children('.user-status').html().search(/ativo/i) >= 0) {
+            $('#warning-box-message').html('<span id="warning-user">' + GMS.DISABLE_USER_WARNING_MSG + name + '</span> ?');
         } else {
-            $('#warning-box-message').html(' <span id="warning-user">' + GMS.ENABLE_USER_WARNING_MSG + name + '</span> ?');
+            $('#warning-box-message').html('<span id="warning-user">' + GMS.ENABLE_USER_WARNING_MSG + name + '</span> ?');
         }
         // Bind the click behavior to the confirmation button
         $('.warning-box .btn-flat:first-child').unbind().click(function () {
