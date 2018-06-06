60a180742eeca916fd9cb133c5f80c73b7b876d9
Delano Medeiros Beder <delano@dc.ufscar.br>
2018-06-05 21:06:25 -0300

    Popula usuários e grupos (via csv) Usuário x Grupos (atualiza admin ou não)

--- ProjetoREMAR/web-app/js/group/manage-user-group.js
+++ ProjetoREMAR/web-app/js/group/manage-user-group.js
@@ -1,13 +1,18 @@
-
- $(document).ready(function () {
+$(document).ready(function () {
     Materialize.updateTextFields();
     $('.modal-trigger').leanModal();
     $('.tooltipped').tooltip({delay: 50});
 
-    $('input').on('input', function() {
+    $('input').on('input', function () {
         $(this).val(function (_, v) {
             return v.replace(/\s+/g, '');
         });
+    });
+
+    var usersCollection = $(".users-collection");
+
+    $(usersCollection).on("click", ".manage-user", function () {
+        manageAdmin(this);
     });
 
     $('form').validate({
@@ -92,8 +97,37 @@ function highlight(el) {
     $(el).removeClass('input-error');
     $(el).addClass('invalid');
 
-    if($(el).attr("data-done") == "true") {
+    if ($(el).attr("data-done") == "true") {
         $(el).siblings(".suffix").remove();
         $(el).removeAttr("data-done");
     }
+}
+
+function manageAdmin(_this) {
+    var userGroupId = $(_this).attr("data-user-group-id");
+    var option = _this.id.substr(_this.id.charAt(0), _this.id.indexOf("n") + 1);
+    var icon = $(_this).children("i")[0];
+    var adminText = $("#admin-" + userGroupId + "-text");
+    $.ajax({
+        type: 'POST',
+        url: "/user-group/manageAdmin",
+        data: {
+            userGroupId: userGroupId,
+            option: option
+        },
+        success: function () {
+            if (option == "make-admin") {
+                $($(_this).prevUntil(".circle")[1]).html(" (Administrador)").fadeIn(400);
+                $(icon).html("star");
+                _this.id = "remove-admin-" + userGroupId;
+                Materialize.toast("Administrador adicionado!", 1500, "rounded");
+            }
+            else if (option == "remove-admin") {
+                $($(_this).prevUntil(".circle")[1]).fadeOut(400);
+                $(icon).html("star_border");
+                _this.id = "make-admin-" + userGroupId;
+                Materialize.toast("Administrador removido!", 1500, "rounded");
+            }
+        }
+    })
 }
