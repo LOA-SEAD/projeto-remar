c8bf70616d9a1b1146650bc98e4aec1780e9885a
Lucas Yuji S. Aciole <lucas.aciole@gmail.com>
2019-01-28 15:53:56 -0200

    Generate Authentication object mannually

--- ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/ShibbolethController.groovy
+++ ProjetoREMAR/grails-app/controllers/br/ufscar/sead/loa/remar/ShibbolethController.groovy
@@ -1,7 +1,14 @@
 package br.ufscar.sead.loa.remar
 
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+
 class ShibbolethController {
 
+    def AuthenticationManager authenticationManager;
     def springSecurityService
 
     def connect() {
@@ -23,42 +30,45 @@ class ShibbolethController {
         */
         log.info "Starting Shibboleth Authentication for " + request.getAttribute("Shib-eduPerson-eduPersonPrincipalName");
         def redirectUrl = "http://alfa.remar.online/shibboleth/authorize"
-        def user = request.getAttribute("Shib-eduPerson-eduPersonPrincipalName")? User.findByUsername(request.getAttribute("Shib-eduPerson-eduPersonPrincipalName")) : null;
+
+        def user = request.getAttribute("Shib-eduPerson-eduPersonPrincipalName") ? User.findByUsername(request.getAttribute("Shib-eduPerson-eduPersonPrincipalName")) : null;
 
         if (user) {
-			log.info "Successfully logged in using Shibboleth Authentication;"
+            log.info "Successfully logged in using Shibboleth Authentication;"
 
-        	render view: "success", model: [user: user, redirectUrl: redirectUrl]
-        } else{
-        	log.info "Creating new Shibboleth-authenticated user;"
-        	user = new User(
-        		username: request.getAttribute("Shib-eduPerson-eduPersonPrincipalName"),
-                password: request.getAttribute("Shib-Session-ID"),
-                email: request.getAttribute("Shib-inetOrgPerson-mail"),
-                firstName: request.getAttribute("Shib-inetOrgPerson-cn"),
-                lastName: request.getAttribute("Shib-inetOrgPerson-sn"),
-                firsAccess: true,
-                enabled: true
+            render view: "success", model: [user: user, redirectUrl: redirectUrl]
+        } else {
+            log.info "Creating new Shibboleth-authenticated user;"
+            user = new User(
+                    username: request.getAttribute("Shib-eduPerson-eduPersonPrincipalName"),
+                    password: "changeit",
+                    email: request.getAttribute("Shib-inetOrgPerson-mail"),
+                    firstName: request.getAttribute("Shib-inetOrgPerson-cn"),
+                    lastName: request.getAttribute("Shib-inetOrgPerson-sn"),
+                    firsAccess: true,
+                    enabled: true
             )
             user.save(flush: true)
 
             if (!user.hasErrors()) {
-            	UserRole.create user, Role.findByAuthority("ROLE_USER"), true
-            	log.info "Successfully created new Shibboleth-authenticated user;"
-            	log.info "${user.username}, ${user.password}"
-            	render view: "success", model: [user: user, redirectUrl: redirectUrl]
+                UserRole.create user, Role.findByAuthority("ROLE_USER"), true
+                log.info "Successfully created new Shibboleth-authenticated user;"
+                render view: "success", model: [user: user, redirectUrl: redirectUrl]
             } else {
-            	render user.errors
+                render user.errors
             }
         }
-
     }
 
     def authorize(LoginShibboleth user) {
-		log.info "User: ${user.username} issued login with password ${user.password};"
+		log.info "User: ${user.username} issued Shibboleth login;"
 
-		springSecurityService.reauthenticate(user.username)
-        session.user = User.findByUsername(user.username)
+        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(user.username, "changeit");
+        Authentication auth = authenticationManager.authenticate(authReq);
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+
+        session.user = User.findByUsername(user.username);
 
 		log.info "Shibboleth flow succesfully authorized;"
 
@@ -69,5 +79,4 @@ class ShibbolethController {
 
 class LoginShibboleth {
 	String username;
-	String password;
 }
