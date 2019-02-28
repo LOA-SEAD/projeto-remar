177ff44b33fb3ba74c868f04c3812c1eccae731a
Lucas Yuji S. Aciole <lucas.aciole@gmail.com>
2019-02-28 17:00:22 -0300

    Remove description field from Tile

--- Memoria/grails-app/domain/br/ufscar/sead/loa/memoria/Tile.groovy
+++ Memoria/grails-app/domain/br/ufscar/sead/loa/memoria/Tile.groovy
@@ -5,16 +5,10 @@ class Tile {
     long ownerId
     String textA
     String textB
-    String description
-
-    static mapping = {
-        description type: "text"
-    }
 
     static constraints = {
         ownerId     blank: false, nullable: false
         textA       blank: false, nullable: false
         textB       blank: false, nullable: false
-        description blank: false, nullable: false
     }
 }
