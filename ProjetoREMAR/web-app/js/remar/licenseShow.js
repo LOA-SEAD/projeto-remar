72c717920bae48921e77d9006b459510d2d4d2d0
Lucas Yuji S. Aciole <lucas.aciole@gmail.com>
2019-03-13 16:11:29 -0300

    Fix javascript bug

--- ProjetoREMAR/web-app/js/remar/licenseShow.js
+++ ProjetoREMAR/web-app/js/remar/licenseShow.js
@@ -18,12 +18,12 @@ $(function () {
 
         $el
         .empty()
-        .append('<a target='_blank' rel="license" href="' + href + '">'
-            + '<img alt="Creative Commons License" src="' + img + '">'
-            + '</a>');
+        .append('<a target="_blank" rel="license" href="' + href + '">' +
+                '<img alt="Creative Commons License" src="' + img + '">' +
+                '</a>');
 
         if (!$el.hasClass('license-image-only')) {
-            $el.append('Esta obra está licenciada com uma Licença <a target='_blank' rel="license" href="' + href + '">Creative Commons Atribuição-CompartilhaIgual 4.0 Internacional</a>.');
+            $el.append('Esta obra está licenciada com uma Licença <a target="_blank" rel="license" href="' + href + '">Creative Commons Atribuição-CompartilhaIgual 4.0 Internacional</a>.');
         }
     });
 });
