54de542cf6b2f45d347a365e62df6b5f22fe355d
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2019-11-08 11:01:26 -0300

    Mudanças no script Text2Speech

--- MemoriaAcessivel/web-app/scripts/gtts.py
+++ MemoriaAcessivel/web-app/scripts/gtts.py
@@ -4,18 +4,15 @@
 # $1 = input text
 # $2 = output file name
 
-# To Install GTTS
-# execute: apt update; apt install python-pip; pip install setuptools --upgrade; pip install gTTS
+# To Install GTTS and mpg123
+# execute: apt update; apt install python-pip mpg123; pip install setuptools --upgrade; pip install gTTS
 
 import subprocess
 import codecs
 import sys
 
-command = 'gtts-cli \'{0}\' --lang pt-br --output {1}'
+command = 'gtts-cli \'{0}\' --lang pt-br --output tmp.mp3; mpg123 -w {1} tmp.mp3 > /dev/null 2>&1; rm tmp.mp3'
 params = sys.argv
-print params
 command = command.format(params[1], params[2])
 print command
 subprocess.call(command, shell=True)
-
-
