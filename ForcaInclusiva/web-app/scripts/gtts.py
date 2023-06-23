41ab182c9cc06c162f9d078356a58b9493b07bcf
Delano Beder <delanobeder@yahoo.com.br>
2023-06-23 09:45:22 -0300

    Atualização da imagem docker do remar Mudança nas permissões dos arquivos (755 => 644) Atualização do script gtts.py

--- ForcaInclusiva/web-app/scripts/gtts.py
+++ ForcaInclusiva/web-app/scripts/gtts.py
@@ -4,10 +4,6 @@
 # $1 = input text
 # $2 = output file name
 
-# To Install GTTS
-# execute: apt update; apt install python3-pip; pip3 install setuptools --upgrade; pip3 install gTTS
-#          update-alternatives --install /usr/bin/python python3 /usr/bin/python3 1
-
 import subprocess
 import codecs
 import sys
@@ -15,5 +11,5 @@ import sys
 command = 'gtts-cli \'{0}\' --lang pt --output {1}'
 params = sys.argv
 command = command.format(params[1], params[2])
-print(command)
+print command
 subprocess.call(command, shell=True)
